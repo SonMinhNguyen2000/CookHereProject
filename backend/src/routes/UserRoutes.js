00bc78a08c11c239c6f78d2533bb43a3ce4f9938
sonminhnguyen2000 <000433430@student.vcc.ca>
2021-11-19 20:01:47 -0800

    comment out test code

--- backend/src/routes/UserRoutes.js
+++ backend/src/routes/UserRoutes.js
@@ -8,8 +8,8 @@ const routes = (app) => {
     app.route('/user/signin')
        .post(controller.addNewUser);
     //get user info route
-    app.route("/user/:userID")
-       .post(controller.getUserWithID);
+   //  app.route("/user/:userID")
+   //     .post(controller.getUserWithID);
    //  //for testing only
    //  app.route("/user")
    //     .get(controller.getAllUser);
